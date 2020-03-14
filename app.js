fa79e7211a99a81bcb89f57c7546255d62f6031d
Satyam1203 <nakulbegana191170@gmail.com>
2020-03-14 17:28:59 +0530

    start script added and include fi

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ var express= require("express"),
     mongoose= require("mongoose"),
     methodOverride = require("method-override"),
     expressSanitizer = require("express-sanitizer");
-    
+
+const checksum_lib = require('./checksum/checksum.js');
 
     mongoose.set('useNewUrlParser', true);
 mongoose.set('useFindAndModify', false);
